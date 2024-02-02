f567f059a7d83837cf28196b7809297c02f83f21
Italo Giovanni Pedroza Guglielmini <italo.guglielmini.3@globalhitss.com.br>
2024-02-02 13:40:37 -0300

    config stryker

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,17 +1,14 @@
-module.exports = function (config) {
-  config.set({
-    mutator: 'typescript',
-    packageManager: 'npm',
-    reporters: ['html', 'clear-text', 'progress'],
-    testRunner: 'jest',
-    transpilers: [],
-    coverageAnalysis: 'off',
-    tsconfigFile: 'tsconfig.json',
-    mutate: ['src/**/*.ts', 'src/**/*.tsx'],
-    jest: {
-      projectType: 'custom',
-      config: require('./jest.config.js'),
-      enableFindRelatedTests: true,
-    },
-  });
+module.exports = {
+  mutator: 'typescript',
+  packageManager: 'npm',
+  reporters: ['html', 'clear-text', 'progress'],
+  transpilers: [],
+  coverageAnalysis: 'off',
+  tsconfigFile: 'tsconfig.json',
+  mutate: ['src/**/*.ts', 'src/**/*.tsx'],
+  jest: {
+    projectType: 'custom',
+    config: require('./jest.config.js'),
+    enableFindRelatedTests: true,
+  },
 };
